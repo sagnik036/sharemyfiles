a9930952cd803753f5c14217d4b33535817ef8d3
Sagnik Banik <iamsagnikbanik@gmail.com>
2022-09-24 16:13:08 +0530

    media url fixed

--- sharemyfile/urls.py
+++ sharemyfile/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
